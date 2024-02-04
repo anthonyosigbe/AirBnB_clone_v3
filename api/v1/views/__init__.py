b21f4829d3ee5104256ff7140547b61536353b4c
Anthony Osigbe <thronesofglory@gmail.com>
2024-02-04 03:47:04 -0800

    7. City tasks completed

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-'''Contains the blueprint for the API.'''
+'''The blueprint for the AirBnB clone's API.'''
 from flask import Blueprint
 
 
@@ -7,11 +7,5 @@ app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 '''The blueprint for the AirBnB clone API.'''
 
 
-from api.v1.views.amenities import *
 from api.v1.views.cities import *
 from api.v1.views.index import *
-from api.v1.views.places_amenities import *
-from api.v1.views.places import *
-from api.v1.views.places_reviews import *
-from api.v1.views.states import *
-from api.v1.views.users import *
